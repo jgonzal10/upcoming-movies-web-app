295354aa8a63ba1f9b1226c3a06ba63481c8b920
JENNIFER GONZALEZ ALZATE <jgonzal10@gmail.com>
2019-06-04 00:05:21 -0300

    deploying

--- server/index.js
+++ server/index.js
@@ -16,12 +16,7 @@ app.use((req, res, next) =>
     )
 app.use(express.static(__dirname + '/dist/movieswebapp'));
 app.use('/api', routes)
-
-app.get('/api/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/movieswebapp/index.html'));
-});
-    
+   
 
 const port = process.env.PORT || 3000;
 
