7183ca918f196c88be0d2d33c2b8e959147d2f31
JENNIFER GONZALEZ ALZATE <jgonzal10@gmail.com>
2019-06-03 23:45:24 -0300

    deploy heroku

--- server/index.js
+++ server/index.js
@@ -9,14 +9,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) =>
     {
-
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next()
     }
     )
+app.use(express.static(path.join(__dirname, '/dist/movieswebapp')));  
 app.use('/api', routes)
-const port = process.env.PORT;
+
+    
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=> 
 console.log(`Movie Web App port is ${process.env.PORT}`))
