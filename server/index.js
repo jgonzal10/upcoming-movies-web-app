71380169dab5bd6bf5e2024f96f934ca934cc934
JENNIFER GONZALEZ ALZATE <jgonzal10@gmail.com>
2019-06-04 19:08:02 -0300

    filter

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) =>
         next()
     }
     )
-// app.use(express.static(__dirname + '/dist/movieswebapp'));
+app.use(express.static(__dirname + '/dist/movieswebapp'));
 
 
 // app.get('*', function(req,res) {
