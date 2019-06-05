91d2336b39ff2d66211fc5c1b0cc934d2ba38c90
JENNIFER GONZALEZ ALZATE <jgonzal10@gmail.com>
2019-06-05 13:29:33 -0300

    deploy

--- server/index.js
+++ server/index.js
@@ -7,16 +7,10 @@ const { PORT } = require('./config')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use((req, res, next) =>
-//     {
-//         res.header("Access-Control-Allow-Origin", "*");
-//         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//         next()
-//     }
-//     )
+
+console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV === 'production') {
-    console.log('in server prod')
     app.use(express.static(path.join(__dirname, '../movieswebapp/dist/movieswebapp')));
 }
 
